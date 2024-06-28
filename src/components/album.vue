472093090c7f0bcd3c9558109d9df6ce26d8a83c
Javier Izquierdo <izquierdovi@esat-alumni.com>
2024-06-28 17:57:23 +0200

    update highlights

--- src/components/album.vue
+++ src/components/album.vue
@@ -47,7 +47,37 @@ function windowResize() {
     <TabItem title="High Lights">
       <template v-slot:default>
         <Copperfield v-bind="json[0]">
-          <template v-slot:default> </template>
+          <template v-slot:default>
+            <p class="pt-3">
+              This is the first published game I've been part of. In it, I've been able to experience and learn what
+              it's like to work with multiple team members with different skill sets and roles. We were a total of 16
+              members and 2 producers.
+            </p>
+            <h4 class="pe-4 pt-4 pb-2">Features and Tasks I was in charge of</h4>
+            <ul>
+              <li><b>Final Boss:</b> This boss had 5 different attacks and 5 different phases. These phases modified the
+                behavior and the attacks he could perform, as well as the size and speed of some of them.
+              </li>
+              <li><b>Performance Optimization:</b> I was in charge of measuring times and implementing solutions to performance
+                problems, such as a custom system to load and unload some expensive actors during gameplay, or finding
+                lumen parameters that increased the performance without sacrificing the visuals.
+              </li>
+              <li><b>Custom Graphics Settings:</b> The default settings that Unreal gives by default treated the visuals very
+                poorly on mid and low presets. So, I was in charge of finding which commands I needed to still lower the
+                requirements while keeping it visually appealing.
+              </li>
+              <li><b>Destructibles:</b> I dealt with Unreal's Chaos Physics to create objects that the player could break, like
+                boxes or vases.
+              </li>
+              <li><b>Non-Playable Character:</b> As a secondary task, the player can push a button to open Robin's cage and
+                free him, which will trigger a dialogue and reward you with coins.
+              </li>
+              <li><b>Coins and Pickups:</b> I made all the behavior of these coins which drop from enemies when hit, similarly
+                to the 3D Sonic games.
+              </li>
+            </ul>
+            <h3></h3>
+          </template>
         </Copperfield>
         <Copperfield v-bind="json[1]">
           <template v-slot:default>
@@ -99,7 +129,8 @@ function windowResize() {
 body {
   padding: 10px;
 }
-.nav-pills > li > a {
+
+.nav-pills>li>a {
   border-radius: 4px 4px 0 0;
 }
 
