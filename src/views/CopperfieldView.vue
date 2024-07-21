0741a99e7b35b095bbe5c6fe14b25e7c9f33370c
Ja <pmijoijab@gmail.com>
2024-07-21 22:06:06 +0200

    final boss text

--- src/views/CopperfieldView.vue
+++ src/views/CopperfieldView.vue
@@ -9,10 +9,98 @@ import DetailLayout from "@/components/layouts/DetailLayout.vue";
     p-data="Katto"
     p-subtitle="Implementation details and decisions"
   >
-    <p></p>
+    <p>
+      Copperfield Engine is a 3D game engine created. It aims to offer the
+      essential features for game development, with an easy-to-use API that
+      follows a data-driven design with the use of ECS, The only libraries used
+      are glfw, glew, imgui, tinyobjloader, StbImage and OpenAL. We have tried
+      to keep them to a minimum, having our own math and collision library, as
+      well as physics.
+    </p>
   </DetailLayout>
+  <SectionLayout p-title="Entity Component System" p-data="ECS">
+    <p>
+      ECS is an architecture that allows to update objects in a scene in an
+      efficient and structured way. This is achieved by following a data-driven
+      design, where components hold data and only data, while the systems
+      operate on the components' data. This separates data from functionality,
+      creating a structured and cache-friendly design since the components can
+      be allocated contiguously in memory with an array or vector.
+    </p>
+    <h3>Efficient Component Management</h3>
+    <p>
+      In the first implementation I made, there was a vector for each type of
+      component, and each vector had the same size as the total number of
+      entities, so the vectors had empty slots when an entity didn't have that
+      type of component. This is the most efficient way when a type of component
+      is used by most of the entities, but if a component is only used by a few
+      entities, the system will still need to go through all the empty slots and
+      do nothing.
+    </p>
+    <p>
+      A compact list holding only the existing components along with the entity
+      they belong to is more efficient in most situations, but not in all
+      situations. Using inheritance, I managed to make both types of containers
+      work under the same rules. To achieve this, I also had to make use of
+      templates that allowed the creation of classes that could hold a vector of
+      any kind of component, and custom iterators for those classes, which
+      allowed a standard way for the systems to go through all the components.
+    </p>
+    <h3>Simplifying Systems</h3>
+    <p>
+      With the component containers done, the users could already create their
+      own custom systems, but to do so, they would have to retrieve all the list
+      iterators and iterate through them while checking that all components
+      existed and belonged to the same entity. I wanted a better solution for
+      this since it was quite a large piece of code that users would probably be
+      copying and pasting, which usually leads to bugs and tedious maintenance.
+    </p>
+    <p>
+      I tried several options and hit several dead ends. In the end, I managed
+      to create a templated class that on construction could receive any number
+      of lists of any type. The class then had an internal function that would
+      iterate through all the lists until an entity with all the components was
+      found. That function could be used as a while loop condition. Lastly,
+      there was another function that would use the iterators from the previous
+      function and form a tuple with the components and return it.
+    </p>
+    <p>
+      Doing just these two functions took me weeks, since I had never worked
+      with parameter packs, custom iterators, and lambdas before. I
+      underestimated how hard this would be, and the first times I tried, I had
+      to give up and start over because I was trying to make the final solution
+      directly, with no intermediate steps or simplifications. This was clearly
+      a mistake to learn from.
+    </p>
+  </SectionLayout>
   <SectionLayout p-title="Physics" p-data="Physics">
-    <p></p>
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h3>Unit testing using blender</h3>
+    <h3>collision worked on first attempt (no, they didn't)</h3>
+  </SectionLayout>
+  <SectionLayout p-title="Input" p-data="Input">
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h3>Controller support</h3>
+  </SectionLayout>
+  <SectionLayout p-title="Rendering" p-data="Rendering">
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h3>Refactoring renderer, again, and again, and again</h3>
+  </SectionLayout>
+  <SectionLayout p-title="Asset Loading" p-data="Asset">
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h3>Asset storing and retrieving</h3>
   </SectionLayout>
 </template>
 
--- src/views/KattoView.vue
+++ src/views/KattoView.vue
@@ -10,145 +10,164 @@ import SectionLayout from "@/components/layouts/SectionLayout.vue";
     p-subtitle="Implementation details and decisions"
   >
     <p>
-      The game was developed using Unreal Engine 5, and programmed with C++,
-      there are a few small functions and classes that were done with blueprints
-      that were mostly made by the end of the development when we needed to rush
-      small details. We always had in mind that whatever we coded should be as
-      usable as possible for our designers or artists.
+      The game was developed using Unreal Engine 5 and programmed with C++.
+      There are a few small functions and classes that were done with
+      blueprints, which were mostly made towards the end of development when we
+      needed to rush small details. We always kept in mind that whatever we
+      coded should be as usable as possible for our designers and artists.
     </p>
     <p>
       During the development, aside from my assigned programming tasks, I played
-      a leader role, acting as a communication bridge between programmers and
-      non programmers. I also frequently found myself helping artists importing
-      and organizing assets on the engine, and with version control. As well as
-      helping the programmers with referencing and using functions and classes
-      done by others.
+      a leadership role, acting as a communication bridge between programmers
+      and non-programmers. I also frequently found myself helping artists import
+      and organize assets in the engine and with version control. Additionally,
+      I assisted programmers with referencing and using functions and classes
+      created by others.
     </p>
   </DetailLayout>
   <SectionLayout p-title="Final Boss" p-data="Final Boss">
     <h3>Behaviour</h3>
     <p>
-      The boss has 5 attacks: Shots with his crossbow, hits on the ground that
-      generate waves, a combo that mixes both previous attacks, a tail swipe
-      that hits if you are near, and a charge. This Charge is the main attack,
-      in it the boss will launch towards the player, and in if the player avoids
-      it he can crash with a pilar, which will stun him for a while, while stunt
-      the player can jum on him to deal damage and make him get to the next
+      The boss has 5 attacks: shots with his crossbow, hits on the ground that
+      generate waves, a combo that mixes the previous two attacks, a tail swipe
+      that hits if you are near, and a charge. This charge is the main attack,
+      in which the boss will launch towards the player. If the player avoids it,
+      the boss can crash into a pillar, which will stun him for a while. While
+      stunned, the player can jump on him to deal damage and advance to the next
       phase.
     </p>
     <p>
-      each phase modifies the attack that can be performed and sometimes the
-      speed and size of them. The first phase is just a charge attack toward a
-      pilar (used as a tutorial). the second only uses crossbow attacks, the
-      third uses waves, the fourth combos and the last uses all of the attacks
-      with slight variations.at any point a melee attack is done if the player
-      is near.
+      Each phase modifies the attacks that can be performed and sometimes the
+      speed and size of them. The first phase is just a charge attack towards a
+      pillar (used as a tutorial). The second only uses crossbow attacks, the
+      third uses waves, the fourth uses combos, and the last uses all of the
+      attacks with slight variations. At any point, a melee attack is performed
+      if the player is near.
     </p>
     <h3 class="pt-4">Challenges</h3>
     <h4 class="challenge pt-0">Deadline-Driven Refactoring</h4>
     <p>
-      At the beginning all of the code for the boss, including internal
-      variables and attacks were on the same class, which was fine at the
-      beginning but most of the boss was done at the last month of the
-      development.
+      At the beginning, all of the code for the boss, including internal
+      variables and attacks, was in the same class. This was fine at the start,
+      but most of the boss was completed in the last month of development.
     </p>
     <p>
-      With the given time I needed to make an implementation fast so that the
-      designers could test and propose improvements before it was too late. And
-      so I did but soon, I was finding myself modifying a small detail and
-      breaking something else that was unrelated. So as soon as I had a barely
-      functional boss that the designers could test on, I started refactoring
-      and moving the code for each attack to an independent component. After
-      that, I had to quickly get back to improve the functionality the code was
-      still far from perfect but it was enough to make the changes designers
-      proposed comfortably and without many new bugs.
+      Given the time constraints, I needed to implement a solution quickly so
+      that the designers could test and propose improvements before it was too
+      late. but while doing so It was very common to add a simple feature and
+      break something unrelated. As soon as I had a barely functional boss that
+      the designers could test, I started refactoring and moving the code for
+      each attack to an independent component. After that, I quickly returned to
+      improving the functionality. the code was still far from perfect but it
+      was enough to make the changes designers proposed comfortably and without
+      introducing many new bugs.
     </p>
     <h4 class="challenge">Node-Based Phase Management</h4>
     <p>
-      initially there was a single phase, when the boss was hitted he would just
-      lose health, the phases were introduced, again, at the last month, so I
-      had to come up with a system that allowed the designers to modify several
-      variables depending on the phase.
+      Initially, there was a single phase where the boss would simply lose
+      health when hit. The phases were introduced, again, in the last month, so
+      I had to come up with a system that allowed the designers to modify
+      several variables depending on the phase.
     </p>
     <p>
-      The way I did it, was by setting the variables through the nodes of the
-      behaviour tree instead of the character blueprint. Inside the behaviour
-      tree I had one subtree for each phase, and the first node executed on each
-      subtree was just to set variables. Aside from that node each attack node
-      had some variables to modify the specific attack
+      I achieved this by setting the variables through the nodes of the behavior
+      tree instead of the character blueprint. Inside the behavior tree, I had
+      one subtree for each phase, and the first node executed in each subtree
+      was just to set variables. Aside from that node, each attack node had some
+      variables to modify the specific attack.
     </p>
     <p>
-      I'm happy with how the system ended but it did lack the ability to set
-      some variables for all the phases, it sometimes happened that we wanted to
-      change the charge speed for every phase and we had to modify the 5
-      different nodes. I could have probably added a base node to set the
-      variables, and only set the phase nodes if the value was modified or
-      adding a tick to each variable to indicate it when the values should be
-      overridden.
+      I'm happy with how the system turned out, but it did lack the ability to
+      set some variables for all the phases. Sometimes we wanted to change the
+      charge speed for every phase and had to modify the 5 different nodes. I
+      could have probably added a base node to set the variables and only set
+      the phase nodes if the value was modified or added a tick to each variable
+      to indicate when the values should be overridden.
     </p>
     <h4 class="challenge">The Wave Dilemma</h4>
     <p>
-      making this attack was pretty difficult, We came up with a good solution
-      for the collision and logic, but for displaying it we couldn't find a
+      Making this attack was pretty difficult. We came up with a good solution
+      for the collision and logic, but for displaying it, we couldn't find a
       solution that didn't show visible seams or artifacts along the wave.
     </p>
     <p>
-      for the collisions I first thought of checking the distance between the
+      For the collisions, I first thought of checking the distance between the
       player and the center of the wave, but that would be pretty hard to update
-      and sync with the visible wave. But since the wave only needed to collide
-      with the player, I could just have small box collider that moved along
-      with the wave and rotated to be always in the player direction. And that
-      worked like a charm.
+      and sync with the visible wave. But I realized, that since the wave only
+      needed to collide with the player, I could just have a small box collider
+      that moved along with the wave and rotated to always face the player's
+      direction. And that worked like a charm.
     </p>
     <p>
-      for the visuals we discussed several options with the artists, like having
-      several small meshes spawn as the wave grows, which would make hard to
-      hide the spawning meshes, another option was to have a growing texture on
-      the ground, but the scaling of it would affect the width of the wave, and
-      thus the gameplay. We couldn't come to and agreement at first, so I
-      started with the most simple, functional solution which was using 4 meshed
-      deformed by a spline forming a circle, this had the problem that applied
-      textures, would either be stretched o create visible seams.
+      For the visuals, we discussed several options with the artists, such as
+      having several small meshes spawn as the wave grows, which would make it
+      hard to hide the spawning meshes. Another option was to have a growing
+      texture on the ground, but the scaling of it would affect the width of the
+      wave and thus the gameplay. We couldn't come to an agreement at first, so
+      I started with a simple and functional solution, which was using 4 meshes
+      deformed by a spline forming a circle. This had the problem that applied
+      textures would either be stretched or create visible seams.
     </p>
     <p>
-      In the end, the assets for the wave were getting constantly delayed and we
-      didn't test many other solutions so this solution stayed, and there are
+      In the end, the assets for the wave were constantly delayed and we didn't
+      test many other solutions, so this solution stayed, and we ended up with
       visible seams on the wave.
     </p>
   </SectionLayout>
 
   <SectionLayout p-title="Destructible objects" p-data="Destuctibles">
-    <h3>Behaviour</h3>
-    <p>WIP</p>
-    <h3>Challenges</h3>
-    <h4 class="challenge">Chaos bug solving</h4>
-    <p>WIP</p>
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h4 class="challenge">Circumventing Chaos Bugs</h4>
+    <!--    <h3>Behaviour</h3>-->
+    <!--    <p>WIP</p>-->
+    <!--    <h3>Challenges</h3>-->
+    <!--    <h4 class="challenge">Chaos bug solving</h4>-->
+    <!--    <p>WIP</p>-->
   </SectionLayout>
 
   <SectionLayout p-title="Optimizations" p-data="Optimizations">
-    <h3>Loading and Unloading Levels</h3>
-    <p>WIP</p>
-    <h3>Optimizing Lumen</h3>
-    <p>WIP</p>
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h4 class="challenge">Loading and Unloading Levels</h4>
+    <h4 class="challenge">Optimizing Lumen</h4>
+    <!--    <h3>Loading and Unloading Levels</h3>-->
+    <!--    <p>WIP</p>-->
+    <!--    <h3>Optimizing Lumen</h3>-->
+    <!--    <p>WIP</p>-->
   </SectionLayout>
 
   <SectionLayout p-title="Pickups and Coins" p-data="Pickups">
-    <h3>Behaviour</h3>
-    <p>WIP</p>
-    <h3>Challenges</h3>
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
     <h4 class="challenge">Projectile Move Component vs physics</h4>
-    <p>WIP</p>
     <h4 class="challenge">The unnoticed visual bug</h4>
-    <p>WIP</p>
+    <!--    <h3>Behaviour</h3>-->
+    <!--    <p>WIP</p>-->
+    <!--    <h3>Challenges</h3>-->
+    <!--    <h4 class="challenge">Projectile Move Component vs physics</h4>-->
+    <!--    <p>WIP</p>-->
+    <!--    <h4 class="challenge">The unnoticed visual bug</h4>-->
+    <!--    <p>WIP</p>-->
   </SectionLayout>
   <SectionLayout p-title="NPC" p-data="PickNPCups">
-    <h3>Behaviour</h3>
-    <p>WIP</p>
-    <h3>Challenges</h3>
-    <h4 class="challenge">Look at the player</h4>
-    <p>WIP</p>
+    <p>
+      I'm still writing this, but here is a title for some of the challenges I
+      faced
+    </p>
+    <h4 class="challenge">Turn the body to face the player</h4>
+    <!--    <h3>Behaviour</h3>-->
+    <!--    <p>WIP</p>-->
+    <!--    <h3>Challenges</h3>-->
+    <!--    <h4 class="challenge">Look at the player</h4>-->
+    <!--    <p>WIP</p>-->
   </SectionLayout>
-
 </template>
 
 <style scoped>
